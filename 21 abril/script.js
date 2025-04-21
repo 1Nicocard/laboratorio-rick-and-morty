273f0dd44dd9721f14d6606e4fc06bfa38c640b9
Nicolas Cardenas Guaca <1109187838@u.icesi.edu.co>
2025-04-21 08:41:46 -0500

    Se crearon las publicacione

---
+++ 21 abril/script.js
@@ -0,0 +1,24 @@
+const galeria = document.querySelector("#galeria");
+
+for (let index = 1; index < 826; index++) {
+  fetch("https://rickandmortyapi.com/api/character/" + index)
+
+      const div = document.createElement("div");
+      div.classList.add("character");
+
+      const elemento = document.createElement("img");
+      elemento.setAttribute("src", "https://rickandmortyapi.com/api/character/avatar/" + index + ".jpeg");
+      div.append(elemento);
+
+      const btnFavoritos = document.createElement("text");
+      btnFavoritos.innerText = "Characater " + index;
+      div.append(btnFavoritos);
+
+      const btnFavorito = document.createElement("text2");
+      btnFavorito.innerText = "Esta es mi publicacion numero " + index +  ". Esta es una prueba de generacion de relleno por medio de funciones y ciclos.";
+      div.append(btnFavorito);
+
+      galeria.append(div);
+    };
+
+ 
